26c1431a461d21c12b48e8e0867080cdacd91c3a
ZachKetner <87997905+ZachKetner@users.noreply.github.com>
2022-09-02 20:57:47 -0500

    through create new

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducers,
+    composeEnhancers(applyMiddleware())
+);
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+root.render(
     <Provider store={store}>
         <App />
-    </Provider>, 
-    document.querySelector('#root')
+    </Provider>
 );
